8307a794d176e96a8a1079983445eefe6553999c
Filip Pękala <272985@student.pwr.edu.pl>
2025-07-21 15:27:17 +0200

    Use OSC52 to yank strings

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,25 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+local function paste()
+	return {
+		vim.fn.split(vim.fn.getreg(""), "\n"),
+		vim.fn.getregtype(""),
+	}
+end
+
+vim.g.clipboard = {
+	name = "OSC 52",
+	copy = {
+		["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+		["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+	},
+	paste = {
+		["+"] = paste,
+		["*"] = paste,
+	},
+}
+
 vim.opt.mouse = ""
 vim.opt.spelllang = {}
 vim.opt.wrap = true
