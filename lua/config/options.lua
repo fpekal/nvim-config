c3a808d58d01d42df29c6d0876a32d931def85ec
Filip Pękala <272985@student.pwr.edu.pl>
2025-10-21 13:46:29 +0200

    Fix clipboard on everest

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -9,19 +9,19 @@ local function paste()
 	}
 end
 
--- if vim.fn.exists("g:neovide") == 0 then
--- 	vim.g.clipboard = {
--- 		name = "OSC 52",
--- 		copy = {
--- 			["+"] = require("vim.ui.clipboard.osc52").copy("+"),
--- 			["*"] = require("vim.ui.clipboard.osc52").copy("*"),
--- 		},
--- 		paste = {
--- 			["+"] = paste,
--- 			["*"] = paste,
--- 		},
--- 	}
--- end
+if not vim.g.neovide then
+	vim.g.clipboard = {
+		name = "OSC 52",
+		copy = {
+			["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+			["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+		},
+		paste = {
+			["+"] = paste,
+			["*"] = paste,
+		},
+	}
+end
 
 vim.opt.mouse = ""
 vim.opt.spelllang = {}
