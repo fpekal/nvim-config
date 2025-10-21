8db19de275f98085cad0913abf85bbd5144757cc
Filip Pękala <272985@student.pwr.edu.pl>
2025-10-21 13:46:29 +0200

    Typst port setup

--- lua/plugins/typst.lua
+++ lua/plugins/typst.lua
@@ -3,6 +3,9 @@ return {
 		"chomosuke/typst-preview.nvim",
 		lazy = false, -- or ft = 'typst'
 		version = "1.*",
-		opts = {}, -- lazy.nvim will implicitly calls `setup {}`
+		opts = {
+			port = 57182,
+			-- open_cmd = "",
+		}, -- lazy.nvim will implicitly calls `setup {}`
 	},
 }
