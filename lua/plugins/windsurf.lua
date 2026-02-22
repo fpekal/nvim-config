06898b1ca65c79a09d426d1ad1bca3da141a7410
Filip Pękala <272985@student.pwr.edu.pl>
2026-02-22 19:45:08 +0100

    Add AI for inline-completion

---
+++ lua/plugins/windsurf.lua
@@ -0,0 +1,16 @@
+return {
+	"Exafunction/windsurf.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"saghen/blink.cmp",
+	},
+	config = function()
+		require("codeium").setup({
+			enable_cmp_source = false,
+			virtual_text = {
+				enabled = true,
+			},
+			default_filetype_enabled = true,
+		})
+	end,
+}
