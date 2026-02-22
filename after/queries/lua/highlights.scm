e65f389797800d3a6434bea05a50af4f5b739d90
Filip Pękala <272985@student.pwr.edu.pl>
2026-02-22 19:45:08 +0100

    Some random after/ directory

---
+++ after/queries/lua/highlights.scm
@@ -0,0 +1,15 @@
+;; extends 
+
+(("in" @keyword) (#set! conceal "i"))
+(("local" @keyword) (#set! conceal "l"))
+(("else" @conditional) (#set! conceal "e"))
+(("elseif" @conditional) (#set! conceal "e"))
+(("return" @keyword.function) (#set! conceal "R"))
+(("then" @conditional) (#set! conceal "t"))
+(("if" @conditional) (#set! conceal "?"))
+(("end" @keyword.function) (#set! conceal "E"))
+(("do" @repeat) (#set! conceal "d"))
+(("for" @repeat) (#set! conceal "F"))
+(("function" @keyword.function) (#set! conceal "f"))
+((function_call name: (identifier) @function.builtin (#eq? @function.builtin "require")) (#set! conceal "r"))
+(("and" @keyword.function) (#set! conceal "&"))
